33922f2229eb880b878068e20195daf0d7e635c6
Thomas Orgeval <orgevalthomas@gmail.com>
2021-05-07 12:11:25 +0200

    adapt size

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from os import listdir, remove, path
+import easygui as easygui
 from PIL import Image
 from tkinter import Tk
 from tkinter.filedialog import askdirectory
 from pathlib import Path
+from os import listdir, remove, path
 
 Tk().withdraw()
 filename = askdirectory()
 images = [file for file in listdir(filename) if file.endswith(('jpeg', 'png', 'jpg'))]
+size = easygui.enterbox("Size of image (x,y)")
+x = int(size.split(',')[0])
+y = int(size.split(',')[1])
 
 Path(filename + '/../' + 'resized').mkdir(parents=True, exist_ok=True)
 for f in listdir(filename + '/../' + 'resized'):
@@ -15,5 +19,5 @@ for f in listdir(filename + '/../' + 'resized'):
 for image in images:
     print(image)
     img = Image.open(filename + '/' + image)
-    img.thumbnail((600, 200))
+    img.thumbnail((x, y))
     img.save("resized/" + image, optimize=True, quality=100)
